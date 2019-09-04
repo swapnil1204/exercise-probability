e8cf125dcc445d04336aa30c82cedd3b2502f145
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-09-04 06:15:18 +0530

    [swapnil] Refactor. static imports added

--- src/test/java/com/thoughtworks/bootcamp/ProbabilityTest.java
+++ src/test/java/com/thoughtworks/bootcamp/ProbabilityTest.java
@@ -3,17 +3,20 @@ package com.thoughtworks.bootcamp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ProbabilityTest {
     @Test
     void givenEqualProbabilityOfTwoEvents_WhenEqual_ThenItShouldReturnTrue(){
         Probability probabilityOfEventHeadWhenCoinToss = new Probability(0.5f);
         Probability probabilityOfEventOneWhenDiceThrown = new Probability(0.5f);
-        Assertions.assertTrue(probabilityOfEventHeadWhenCoinToss.equals(probabilityOfEventOneWhenDiceThrown));
+        assertTrue(probabilityOfEventHeadWhenCoinToss.equals(probabilityOfEventOneWhenDiceThrown));
     }
     @Test
-    void givenEqualProbabilityOfTwoEvents_WhenEqual_ThenItShouldReturnFalse(){
+    void givenUnEqualProbabilityOfTwoEvents_WhenEqual_ThenItShouldReturnFalse(){
         Probability probabilityOfEventHeadWhenCoinToss = new Probability(0.5f);
         Probability probabilityOfEventOneWhenDiceThrown = new Probability(0.4f);
-        Assertions.assertFalse(probabilityOfEventHeadWhenCoinToss.equals(probabilityOfEventOneWhenDiceThrown));
+        assertFalse(probabilityOfEventHeadWhenCoinToss.equals(probabilityOfEventOneWhenDiceThrown));
     }
 }
