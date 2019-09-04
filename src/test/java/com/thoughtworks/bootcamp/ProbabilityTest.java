27f405660aa6ccfb8eee4757a7ade6044c4b7e43
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-09-04 16:09:19 +0530

    [swapnil] Refactor. Changing name

--- src/test/java/com/thoughtworks/bootcamp/ProbabilityTest.java
+++ src/test/java/com/thoughtworks/bootcamp/ProbabilityTest.java
@@ -1,39 +1,50 @@
 package com.thoughtworks.bootcamp;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ProbabilityTest {
+    private static Probability probabilityOfHeadWhenCoinToss;
+    private static Probability probabilityOfEvenNumberWhenDiceThrown;
+    private static Probability probabilityOfEventHeadWhenCoinToss = new Probability(0.5f);
+    private static Probability probabilityOfEventOneWhenDiceThrown;
+
+    @BeforeAll
+    static void setup(){
+        probabilityOfHeadWhenCoinToss = new Probability(0.5f);
+        probabilityOfEvenNumberWhenDiceThrown = new Probability(0.5f);
+        probabilityOfEventHeadWhenCoinToss = new Probability(0.5f);
+        probabilityOfEventOneWhenDiceThrown = new Probability(0.4f);
+        Probability probabilityOfEventHeadWhenCoinToss1 = new Probability(0.7f);
+    }
     @Test
     void givenEqualProbabilityOfTwoEvents_WhenEqual_ThenItShouldReturnTrue(){
-        Probability probabilityOfEventHeadWhenCoinToss = new Probability(0.5f);
-        Probability probabilityOfEventOneWhenDiceThrown = new Probability(0.5f);
-        assertTrue(probabilityOfEventHeadWhenCoinToss.equals(probabilityOfEventOneWhenDiceThrown));
+        assertTrue(probabilityOfHeadWhenCoinToss.equals(probabilityOfEvenNumberWhenDiceThrown));
     }
     @Test
     void givenUnEqualProbabilityOfTwoEvents_WhenEqual_ThenItShouldReturnFalse(){
-        Probability probabilityOfEventHeadWhenCoinToss = new Probability(0.5f);
-        Probability probabilityOfEventOneWhenDiceThrown = new Probability(0.4f);
         assertFalse(probabilityOfEventHeadWhenCoinToss.equals(probabilityOfEventOneWhenDiceThrown));
     }
     @Test
     void givenProbabilityHeadWhenCoinToss_WhenCheckNoOccurence_ThenItShouldReturnPointFive(){
-        Probability probabilityOfEventHeadWhenCoinToss = new Probability(0.5f);
         assertEquals(0.5f,probabilityOfEventHeadWhenCoinToss.checkOccurence());
     }
     @Test
     void givenProbabilityOfHeadWhenCoinTossIsPointSeven_WhenCheckNoOccurence_ThenItShouldReturnPointFive(){
-        Probability probabilityOfEventHeadWhenCoinToss = new Probability(0.7f);
-        assertEquals(0.3f,probabilityOfEventHeadWhenCoinToss.checkOccurence());
+        assertEquals(0.3f,probabilityOfEventHeadWhenCoinToss1.checkOccurence());
     }
-    @Test
-    void givenProbabilityOfHeadWhenCoinTossAndOddNumberWhenDiceThrown_WhenEventsTogether_ThenItShouldReturnExpectedResult(){
-        float ProbabilityOfHeadWhenCoinToss = 2.5f;
-        float ProbabilityOfEvenNumberWhenDiceThrown = 2.5f;
-        Probability probability = new Probability(ProbabilityOfHeadWhenCoinToss);
-        Probability anotherProbabilityOfEvent = new Probability(ProbabilityOfEvenNumberWhenDiceThrown);
-        assertEquals(6.25f, probability.eventsTogether(anotherProbabilityOfEvent));
-    }
+//    @Test
+//    void givenProbabilityOfHeadWhenCoinTossAndOddNumberWhenDiceThrown_WhenEventsTogether_ThenItShouldReturnExpectedResult(){
+//        float ProbabilityOfHeadWhenCoinToss = 2.5f;
+//        float ProbabilityOfEvenNumberWhenDiceThrown = 2.5f;
+//        Probability probability = new Probability(ProbabilityOfHeadWhenCoinToss);
+//        Probability anotherProbabilityOfEvent = new Probability(ProbabilityOfEvenNumberWhenDiceThrown);
+//        assertEquals(6.25f, probability.eventsTogether(anotherProbabilityOfEvent));
+//    }
 }
+
